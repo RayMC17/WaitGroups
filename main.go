ab608fafe24412a1f1afaa0b92a7419ea9b1f213
RayMC17 <2021154121@ub.edu.bz>
2023-02-15 13:32:14 -0600

    reversing

--- main.go
+++ main.go
@@ -12,12 +12,28 @@ func main() {
 	msg := flag.String("msg", "Howdy, stranger!", "the message")
 	num := flag.Int("num", 1, "How many times to print the message")
 	caps := flag.Bool("U", false, "Specify whether to uppercase the string(true/false)")
+	reverse:= flag.Bool("r", false, "reverse the string (true /false)")
 
 	flag.Parse()
 	// check if it is should be uppercase before printing it.
 	if *caps {
 		*msg = strings.ToUpper(*msg)
 	}
+	//check if we should reverse the string
+			
+		//reverse string
+		//step:
+		//1. create an empty string var result string
+		//2. Iterate over the string that you want to reverse for _, value := range *msg
+	if *reverse{
+		var result string
+		for _, value := range *msg{
+			result = string(value) + result
+		}
+		//write the reverse string to *msg
+		*msg = result
+	}
+
 	//print the s string
 	for i := 0; i < *num; i++ {
 		fmt.Println(*msg)
