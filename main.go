28ed8a28d297e539c134aeee44e7cc499a6a927c
RayMC17 <2021154121@ub.edu.bz>
2023-02-15 13:06:50 -0600

    Still updating

--- main.go
+++ main.go
@@ -8,16 +8,19 @@ import (
 
 //domnstrate flags
 
-func main(){
+func main() {
 	msg := flag.String("msg", "Howdy, stranger!", "the message")
-	num:= flag.Int("num", 1 ,"How many times to print the message")
-	caps:= flag.Int("num", 1 ,"Should print in all caps")
-	
-	flag.Parse()
+	num := flag.Int("num", 1, "How many times to print the message")
+	caps := flag.Bool("U", false, "Specify whether to uppercase the string(true/false)")
 
-	for i := 0; i < *num; i++{
+	flag.Parse()
+	// check if it is should be uppercase before printing it.
+	if *caps {
+		*msg = strings.ToUpper(*msg)
+	}
+	//print the s string
+	for i := 0; i < *num; i++ {
 		fmt.Println(*msg)
 	}
 
-
-	}
+}
